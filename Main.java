7d1a2e4ccd9405902534c8919afe6706bc7a0575
Shalini8 <shalu8mar@gmail.com>
2021-04-08 23:13:16 +0530

    pass the parameters and call getlength()

--- Main.java
+++ Main.java
@@ -4,9 +4,20 @@ public class Main {
 	public static void main(String[] args) {
 		Point p1 = new Point(4, 5);
 		Point p2 = new Point(10, 2);
+		Point p3 = new Point(4, 5);
+		Point p4 = new Point(10, 2);
 
-	Line l1 = new Line(p1, p2);
-	System.out.println (l1.getLength());	
-	}
-	}
+		Line l1 = new Line(p1, p2);
+		Line l2 = new Line(p3, p4);
 
+		l1.getLength();
+		l2.getLength();
+
+		if (l1.equals(l2)) {
+			System.out.println("both lines are equal");
+		} else {
+			System.out.println("both lines are unequal");
+			
+		}
+	}
+}
